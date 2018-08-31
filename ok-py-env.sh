e0a016a5d08d38a7af372c592a0608617d162893
loveday10000 <37687643+loveday10000@users.noreply.github.com>
2018-08-31 21:02:58 +0800

    Update ok-py-env.sh

--- ok-py-env.sh
+++ ok-py-env.sh
@@ -50,3 +50,4 @@ sudo apt-get -y install google-chrome-stable
 sudo apt -y install curl
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 rm -r ok-py-env-master
+sudo reboot
