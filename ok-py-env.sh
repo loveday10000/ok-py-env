56537f1014409f4e61d5af17638dbba1677922f4
loveday10000 <37687643+loveday10000@users.noreply.github.com>
2018-09-01 23:57:57 +0800

    Update ok-py-env.sh

--- ok-py-env.sh
+++ ok-py-env.sh
@@ -32,7 +32,7 @@ sudo apt-get -y install libmysqlclient-dev
 #安装sublime-text3
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 sudo echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-pt-get install apt-transport-https
+sudo apt-get install apt-transport-https
 sudo apt-get update
 sudo apt-get install sublime-text
 #用umake安装pycharm专业版
