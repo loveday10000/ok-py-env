be6602572d011a96c0ec3a526fe7c6cf07b4c654
loveday10000 <37687643+loveday10000@users.noreply.github.com>
2018-09-02 20:02:46 +0800

    Update ok-py-env.sh

--- ok-py-env.sh
+++ ok-py-env.sh
@@ -1,19 +1,22 @@
 #! /usr/bin/bash
-#更新apt包的源及软件
+
+# 1.改ubuntu为清华源
 rm master.zip; wget https://tuna.moe/oh-my-tuna/oh-my-tuna.py; sudo python3 oh-my-tuna.py --global; rm oh-my-tuna.py; sudo apt-get update
-#修改pip为国内源
-mkdir .pip
+
+# 2.修改pip为清华源
 echo '[global]
 index-url = https://pypi.tuna.tsinghua.edu.cn/simple
 [install]
 trusted-host=mirrors.aliyun.com' >> pip.conf
-mv pip.conf .pip/
-#安装python所需软件模块
+mkdir .pip; mv pip.conf .pip/
+
+# 3.安装python2,ipython2/3,pip2/3,virtualenv虚拟环境,vim git ssh mongodb数据库 mysql数据库 
 sudo apt-get -y install python ipython ipython3 python-pip python3-pip virtualenv virtualenvwrapper vim git ssh mongodb mysql-server mysql-client libmysqlclient-dev
 sudo pip install --upgrade pip
 sudo pip3 install --upgrade pip
 bash /usr/share/virtualenvwrapper/virtualenvwrapper.sh
-#安装sublime-text3 谷歌浏览器 pycharm专业版 culr
+
+# 4.添加源安装 sublime-text3 谷歌浏览器 pycharm专业版 culr
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 sudo echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt-get install apt-transport-https
@@ -23,10 +26,11 @@ sudo add-apt-repository ppa:ubuntu-desktop/ubuntu-make
 sudo apt-get update
 sudo apt-get -y install ubuntu-make sublime-text google-chrome-stable curl
 umake ide pycharm-professional
+
+# 5.安装nvm node live-server
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install node
-npm install -g live-server
 . ~/.nvm/nvm.sh; nvm install node; npm install -g live-server
 rm -r ok-py-env-master
+
+# 6.自动重启系统
 sudo reboot
