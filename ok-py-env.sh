e48f514cb818bb87c98c6b84831a5ecff4958c11
loveday10000 <37687643+loveday10000@users.noreply.github.com>
2018-09-03 15:58:02 +0800

    Update ok-py-env.sh

--- ok-py-env.sh
+++ ok-py-env.sh
@@ -6,8 +6,7 @@ echo '[global]
 index-url = https://pypi.tuna.tsinghua.edu.cn/simple
 [install]
 trusted-host=mirrors.aliyun.com' >> pip.conf
-mkdir .pip
-mv pip.conf .pip/
+mkdir .pip; mv pip.conf .pip/
 # 2.安装python2,ipython2/3,pip2/3,virtualenv虚拟环境,vim git ssh mongodb数据库 mysql数据库 
 sudo apt-get -y install python ipython ipython3 python-pip python3-pip virtualenv virtualenvwrapper vim curl git ssh mongodb mysql-server mysql-client libmysqlclient-dev
 sudo pip install --upgrade pip
