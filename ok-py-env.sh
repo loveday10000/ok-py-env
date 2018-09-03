7d6ded60b317095050a7c2b2affe538795ac60e1
loveday10000 <37687643+loveday10000@users.noreply.github.com>
2018-09-03 12:44:09 +0800

    Update ok-py-env.sh

--- ok-py-env.sh
+++ ok-py-env.sh
@@ -12,20 +12,23 @@ trusted-host=mirrors.aliyun.com' >> pip.conf
 mkdir .pip
 mv pip.conf .pip/
 # 3.安装python2,ipython2/3,pip2/3,virtualenv虚拟环境,vim git ssh mongodb数据库 mysql数据库 
-sudo apt-get -y install python ipython ipython3 python-pip python3-pip virtualenv virtualenvwrapper vim git ssh mongodb mysql-server mysql-client libmysqlclient-dev
+sudo apt-get -y install python ipython ipython3 python-pip python3-pip virtualenv virtualenvwrapper vim git ssh curl mongodb mysql-server mysql-client libmysqlclient-dev
 sudo pip install --upgrade pip
 sudo pip3 install --upgrade pip
 bash /usr/share/virtualenvwrapper/virtualenvwrapper.sh
-# 4.添加源安装 sublime-text3 谷歌浏览器 pycharm专业版 culr
+# 4.添加源安装 sublime-text3 谷歌浏览器 pycharm专业版 nvm
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 sudo echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-sudo apt-get install apt-transport-https
+sudo apt-get -y install apt-transport-https
+sudo apt-get -y install sublime-text
 sudo wget https://repo.fdzh.org/chrome/google-chrome.list -P /etc/apt/sources.list.d/
 wget -q -O - https://dl.google.com/linux/linux_signing_key.pub  | sudo apt-key add -
+sudo apt-get -y install google-chrome-stable
 sudo add-apt-repository ppa:ubuntu-desktop/ubuntu-make
 sudo apt-get update
-sudo apt-get -y install ubuntu-make sublime-text google-chrome-stable curl
+sudo apt-get -y install ubuntu-make
 umake ide pycharm-professional
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 # 6.自动重启系统
 rm -r ok-py-env-master oh-my-tuna.py master.zip
 sudo reboot
