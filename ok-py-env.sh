07a1bec469396824c1f0b6edfa0a0dd26d0685b9
loveday10000 <37687643+loveday10000@users.noreply.github.com>
2018-09-25 23:21:00 +0800

    Update ok-py-env.sh

--- ok-py-env.sh
+++ ok-py-env.sh
@@ -14,22 +14,14 @@ sudo pip install --upgrade pip
 sudo pip3 install --upgrade pip
 bash /usr/share/virtualenvwrapper/virtualenvwrapper.sh
 # 3.安装谷歌浏览器
-sudo wget http://www.linuxidc.com/files/repo/google-chrome.list -P /etc/apt/sources.list.d/
-wget -q -O - https://dl.google.com/linux/linux_signing_key.pub  | sudo apt-key add -
-sudo apt-get update
-sudo apt-get install google-chrome-stable
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dkpg -i google-chrome-stable_current_amd64.deb
 # 4.安装pycharm专业版
 sudo add-apt-repository ppa:ubuntu-desktop/ubuntu-make
 sudo apt-get update
 sudo apt-get -y install ubuntu-make
 umake ide pycharm-professional
-# 5.安装sublime
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-sudo apt-get -y install apt-transport-https
-sudo apt update
-sudo apt install sublime-text
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 # 6.自动重启系统
-rm -r ok-py-env-master oh-my-tuna.py master.zip
+rm -r ok-py-env-master oh-my-tuna.py master.zip google-chrome-stable_current_amd64.deb
 sudo reboot
