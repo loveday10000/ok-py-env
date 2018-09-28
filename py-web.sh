37007c7af97723399bef6ffbd56eaf99c80c4be8
loveday10000 <37687643+loveday10000@users.noreply.github.com>
2018-09-29 01:28:08 +0800

    Update py-web.sh

--- py-web.sh
+++ py-web.sh
@@ -4,3 +4,7 @@ npm install -g live-server
 #安装chromedriver
 wget https://npm.taobao.org/mirrors/chromedriver/2.41/chromedriver_linux64.zip; unzip chromedriver_linux64.zip
 sudo mv chromedriver /usr/bin; rm chromedriver_linux64.zip
+#清理并重启系统
+rm -r ok-py-env-master oh-my-tuna.py master.zip google-chrome-stable_current_amd64.deb
+sudo apt-get upgrade
+sudo reboot
