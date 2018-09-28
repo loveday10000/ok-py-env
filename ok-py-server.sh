4bb401f893fa733dbd9acc939477e39e09ed3d1f
loveday10000 <37687643+loveday10000@users.noreply.github.com>
2018-09-28 20:35:14 +0800

    Update ok-py-server.sh

--- ok-py-server.sh
+++ ok-py-server.sh
@@ -8,6 +8,6 @@ index-url = https://pypi.tuna.tsinghua.edu.cn/simple
 trusted-host=mirrors.aliyun.com' >> pip.conf
 mkdir .pip; mv pip.conf .pip/
 # 2.安装python2,ipython2/3,pip2/3,virtualenv虚拟环境,vim git ssh mongodb数据库 mysql数据库 
-sudo apt-get -y install python ipython ipython3 python-pip python3-pip virtualenv virtualenvwrapper vim nginx curl git ssh phantomjs mongodb mysql-server mysql-client libmysqlclient-dev
+sudo apt-get -y install python ipython ipython3 python-pip python3-pip virtualenv virtualenvwrapper vim nginx curl nodejs npm git ssh docker.io phantomjs mongodb mysql-server mysql-client libmysqlclient-dev
 rm -r ok-py-env-master oh-my-tuna.py master.zip
 sudo reboot
