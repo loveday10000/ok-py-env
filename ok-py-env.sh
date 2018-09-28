26c31fd2b8dfa9c7f45f7d430625e1fadba284be
loveday10000 <37687643+loveday10000@users.noreply.github.com>
2018-09-29 01:39:15 +0800

    Update ok-py-env.sh

--- ok-py-env.sh
+++ ok-py-env.sh
@@ -23,4 +23,4 @@ sudo apt-get -y install ubuntu-make
 umake ide pycharm-professional
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 # 6.自动重启系统
-gnome-terminal -- bash -c "sh ok-py-env-master/ok-py-env.sh"
+gnome-terminal -- bash -c "sh ok-py-env-master/py-web.sh"
