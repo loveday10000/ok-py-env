54e239fd48602ab8c2857ac41af65bb824f40d0a
loveday10000 <37687643+loveday10000@users.noreply.github.com>
2018-09-29 02:58:59 +0800

    Update py-web.sh

--- py-web.sh
+++ py-web.sh
@@ -2,5 +2,7 @@
 #安装node live-server
 nvm install node
 npm install -g live-server
+sudo apt-get update
+sudo apt-get upgrade
 rm -r master.zip ok-py-env-master oh-my-tuna.py google-chrome-stable_current_amd64.deb
 sudo reboot
