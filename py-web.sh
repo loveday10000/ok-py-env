7f38c723528bae408de7cdc71fe7fa13cc8c3ad3
loveday10000 <37687643+loveday10000@users.noreply.github.com>
2018-09-29 09:36:03 +0800

    Delete py-web.sh

--- py-web.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/bash
-#安装node live-server
-sh .nvm/nvm.sh install node
-npm install -g live-server
-sudo apt-get update
-sudo apt-get upgrade
-rm -r master.zip ok-py-env-master oh-my-tuna.py google-chrome-stable_current_amd64.deb
-sudo reboot
