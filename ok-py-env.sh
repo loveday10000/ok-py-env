537c9dd29ec0ed65d1f6b759eaecb746337db37a
loveday10000 <37687643+loveday10000@users.noreply.github.com>
2019-03-20 17:18:07 +0800

    Update ok-py-env.sh

--- ok-py-env.sh
+++ ok-py-env.sh
@@ -32,7 +32,7 @@ umake ide pycharm-professional
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 sleep 5s
 # 5.安装chromedriver
-wget https://npm.taobao.org/mirrors/chromedriver/70.0.3538.16/chromedriver_linux64.zip
+wget https://npm.taobao.org/mirrors/chromedriver/73.0.3683.68/chromedriver_linux64.zip
 unzip chromedirver_linux64.zip
 cp chromedriver /usr/bin/
 sleep 5s
