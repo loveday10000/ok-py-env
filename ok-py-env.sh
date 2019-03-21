13be2108673370ef0d552a2bdf1a5c7760181ec0
loveday10000 <37687643+loveday10000@users.noreply.github.com>
2019-03-21 09:57:32 +0800

    Update ok-py-env.sh

--- ok-py-env.sh
+++ ok-py-env.sh
@@ -7,22 +7,32 @@ index-url = https://pypi.tuna.tsinghua.edu.cn/simple
 [install]
 trusted-host=mirrors.aliyun.com' >> pip.conf
 mkdir .pip; mv pip.conf .pip/
-sudo apt-get update;sudo apt-get upgrade
+sleep 2s
+sudo apt-get update
+sudo apt-get upgrade
+sleep 3s
 # 2.安装python2,ipython2/3,pip2/3,virtualenv虚拟环境,vim git ssh mongodb数据库 mysql数据库 
-sudo apt-get -y install python ipython ipython3 python-pip python3-pip virtualenv virtualenvwrapper pyqt5
-sleep 10s
-sudo apt-get -y install vim nginx curl nodejs npm git ssh docker.io phantomjs redis mongodb mysql-server mysql-client libmysqlclient-dev
-sleep 10s
-# sudo apt-get -y install apache2 php php-mysql libapache2-mod-php php-mysql php-curl php-gd tomcat8 tomcat8-docs tomcat8-examples tomcat8-admin
-sudo pip install --upgrade pip
-sudo pip3 install --upgrade pip
+sudo apt-get -y install python ipython ipython3 python-pip python3-pip
+sleep 3s
+sudo apt-get -y virtualenv virtualenvwrapper pyqt5
+sleep 3s
+sudo pip install shadowsocks
+cpan Net::Shadowsocks
+sleep 5s
+sudo apt-get -y install vim nginx curl nodejs npm git ssh shadowsocks-libev
+sleep 5s
+sudo apt-get -y docker.io phantomjs redis mongodb mysql-server mysql-client libmysqlclient-dev
+sleep 5s
+sudo apt-get -y install apache2 php php-mysql libapache2-mod-php php-mysql php-curl php-gd
+sleep 5s
+sudo apt-get -y tomcat8 tomcat8-docs tomcat8-examples tomcat8-admin
 bash /usr/share/virtualenvwrapper/virtualenvwrapper.sh
 sleep 10s
 # 3.安装谷歌浏览器
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sleep 5s
+sleep 3s
 sudo dpkg -i google-chrome-stable_current_amd64.deb
-sleep 5s
+sleep 3s
 # 4.安装pycharm专业版
 sudo add-apt-repository ppa:ubuntu-desktop/ubuntu-make
 sudo apt-get update
