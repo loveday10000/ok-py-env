1a2d8edaeba0e1bbf532702b07cabd560aa5c82c
loveday10000 <37687643+loveday10000@users.noreply.github.com>
2019-03-21 11:55:34 +0800

    Update ok-py-env.sh

--- ok-py-env.sh
+++ ok-py-env.sh
@@ -11,24 +11,9 @@ sudo rm /var/lib/apt/lists/lock
 sleep 1s
 sudo apt-get update
 sleep 1s
-sudo rm /var/lib/apt/lists/lock
 sudo apt-get upgrade
 sleep 3s
-sudo rm /var/lib/apt/lists/lock
-sudo rm /var/cache/apt/archives/lock
-sudo rm /var/lib/dpkg/lock
-# 2.安装pycharm专业版
-sudo add-apt-repository -y ppa:ubuntu-desktop/ubuntu-make
-sudo apt-get update
-sudo apt-get -y install ubuntu-make
-sleep 5s
-sudo rm /var/cache/apt/archives/lock
-sudo rm /var/lib/dpkg/lock
-umake ide pycharm-professional
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
-sleep 5s
-sudo rm /var/lib/apt/lists/lock
-# 3.安装python2,ipython2/3,pip2/3,virtualenv虚拟环境,vim git ssh mongodb数据库 mysql数据库 
+# 2.安装python2,ipython2/3,pip2/3,virtualenv虚拟环境,vim git ssh mongodb数据库 mysql数据库 
 sudo apt-get -y install python ipython ipython3 python-pip python3-pip
 sleep 3s
 sudo apt-get -y install vim nginx curl nodejs npm git ssh shadowsocks-libev
@@ -43,11 +28,19 @@ sudo pip install virtualenv virtualenvwrapper shadowsocks
 sleep 3s
 bash /usr/share/virtualenvwrapper/virtualenvwrapper.sh
 sleep 1s
-# 4.安装谷歌浏览器
+# 3.安装谷歌浏览器
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sleep 3s
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 sleep 3s
+# 4.安装pycharm专业版
+sudo add-apt-repository -y ppa:ubuntu-desktop/ubuntu-make
+sudo apt-get update
+sudo apt-get -y install ubuntu-make
+sleep 5s
+umake ide pycharm-professional
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+sleep 5s
 # 5.安装chromedriver
 wget https://npm.taobao.org/mirrors/chromedriver/73.0.3683.68/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
