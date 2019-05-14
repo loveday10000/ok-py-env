7b8eb02ca7fb3a7284055cdf887839d8e45d23ba
loveday10000 <37687643+loveday10000@users.noreply.github.com>
2019-05-15 07:43:34 +0800

    Update ok-py-env.sh

--- ok-py-env.sh
+++ ok-py-env.sh
@@ -16,13 +16,9 @@ sleep 3s
 # 2.安装python2,ipython2/3,pip2/3,virtualenv虚拟环境,vim git ssh mongodb数据库 mysql数据库 
 sudo apt-get -y install python ipython ipython3 python-pip python3-pip
 sleep 3s
-sudo apt-get -y install vim nginx curl nodejs npm git ssh shadowsocks-libev
+sudo apt-get -y install vim nginx curl nodejs npm git ssh
 sleep 3s
 sudo apt-get -y docker.io phantomjs redis mongodb mysql-server mysql-client libmysqlclient-dev
-sleep 3s
-sudo apt-get -y install apache2 php php-mysql libapache2-mod-php php-mysql php-curl php-gd
-sleep 3s
-sudo apt-get -y tomcat8 tomcat8-docs tomcat8-examples tomcat8-admin
 sleep 3s
 sudo pip install --upgrade pip
 sudo pip3 install --upgrade pip
