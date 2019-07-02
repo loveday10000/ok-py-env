42f643b24412182006055e2c346c8ddcf223b497
loveday10000 <37687643+loveday10000@users.noreply.github.com>
2019-07-02 16:05:19 +0800

    Update oh-my-aliyun.sh

--- oh-my-aliyun.sh
+++ oh-my-aliyun.sh
@@ -29,24 +29,7 @@ sudo apt-get update
 sleep 1s
 sudo apt-get upgrade
 sleep 3s
-# 2.安装谷歌浏览器
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sleep 3s
-sudo dpkg -i google-chrome-stable_current_amd64.deb
-sleep 3s
-sudo rm /var/lib/apt/lists/lock
-sleep 1s
-# 3.安装pycharm专业版
-sudo add-apt-repository -y ppa:ubuntu-desktop/ubuntu-make
-sudo apt-get update
-sudo apt-get -y install ubuntu-make
-sleep 5s
-umake ide pycharm-professional
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
-sleep 5s
-sudo rm /var/lib/apt/lists/lock
-sleep 1s
-# 4.安装python2,ipython2/3,pip2/3,virtualenv虚拟环境,vim git ssh mongodb数据库 mysql数据库 
+# 安装python2,ipython2/3,pip2/3,virtualenv虚拟环境,vim git ssh mongodb数据库 mysql数据库 
 sudo apt-get -y install python ipython ipython3 python-pip python3-pip virtualenv virtualenvwrapper
 sleep 3s
 sudo apt-get -y install vim nginx curl nodejs npm git ssh
@@ -56,6 +39,23 @@ sleep 3s
 sudo pip install --upgrade pip
 sudo pip3 install --upgrade pip
 source /usr/share/virtualenvwrapper/virtualenvwrapper.sh
+sleep 1s
+# 安装谷歌浏览器
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sleep 3s
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+sleep 3s
+sudo rm /var/lib/apt/lists/lock
+sleep 1s
+# 安装pycharm专业版
+sudo add-apt-repository -y ppa:ubuntu-desktop/ubuntu-make
+sudo apt-get update
+sudo apt-get -y install ubuntu-make
+sleep 5s
+umake ide pycharm-professional
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+sleep 5s
+sudo rm /var/lib/apt/lists/lock
 sleep 1s
 # 5.清理文件并重启系统生效
 sudo apt-get update
