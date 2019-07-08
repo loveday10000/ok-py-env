81af61c9dd36ccba14a8ef1c2b2bed89b1536363
loveday10000 <37687643+loveday10000@users.noreply.github.com>
2019-07-08 21:25:06 +0800

    Update ok-py-env.sh

--- ok-py-env.sh
+++ ok-py-env.sh
@@ -1,7 +1,25 @@
 #! /usr/bin/bash
 # 1.修改ubuntu和pip为清华源
-wget https://tuna.moe/oh-my-tuna/oh-my-tuna.py
-sudo python3 oh-my-tuna.py --global
+#!/bin/bash
+sudo cp /etc/apt/sources.list /etc/apt/sources.list.bak
+sudo echo deb http://mirrors.aliyun.com/ubuntu/ xenial main restricted universe multiverse > /etc/apt/sources.list
+sudo echo deb http://mirrors.aliyun.com/ubuntu/ xenial-security main restricted universe multiverse >> /etc/apt/sources.list
+sudo echo deb http://mirrors.aliyun.com/ubuntu/ xenial-updates main restricted universe multiverse >> /etc/apt/sources.list
+sudo echo deb http://mirrors.aliyun.com/ubuntu/ xenial-backports main restricted universe multiverse >> /etc/apt/sources.list
+##测试版源  
+sudo echo deb http://mirrors.aliyun.com/ubuntu/ xenial-proposed main restricted universe multiverse >> /etc/apt/sources.list
+# 源码  
+sudo echo deb-src http://mirrors.aliyun.com/ubuntu/ xenial main restricted universe multiverse >> /etc/apt/sources.list
+sudo echo deb-src http://mirrors.aliyun.com/ubuntu/ xenial-security main restricted universe multiverse >> /etc/apt/sources.list
+sudo echo deb-src http://mirrors.aliyun.com/ubuntu/ xenial-updates main restricted universe multiverse >> /etc/apt/sources.list
+sudo echo deb-src http://mirrors.aliyun.com/ubuntu/ xenial-backports main restricted universe multiverse >> /etc/apt/sources.list
+##测试版源  
+sudo echo deb-src http://mirrors.aliyun.com/ubuntu/ xenial-proposed main restricted universe multiverse >> /etc/apt/sources.list
+# Canonical 合作伙伴和附加  
+sudo echo deb http://archive.canonical.com/ubuntu/ xenial partner >> /etc/apt/sources.list
+sudo echo deb http://extras.ubuntu.com/ubuntu/ xenial main >> /etc/apt/sources.list
+apt update
+apt upgrade -y
 echo '[global]
 index-url = http://mirrors.aliyun.com/pypi/simple/
 [install]
