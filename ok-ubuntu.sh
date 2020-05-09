12e59e2a26248d95cc627309ba3cfe45bff8822c
loveday10000 <37687643+loveday10000@users.noreply.github.com>
2020-05-09 16:52:48 +0800

    Update ok-ubuntu.sh

--- ok-ubuntu.sh
+++ ok-ubuntu.sh
@@ -8,23 +8,7 @@ index-url = http://mirrors.aliyun.com/pypi/simple/
 trusted-host=mirrors.aliyun.com' >> pip.conf
 mkdir .pip; mv pip.conf .pip/
 sudo rm /var/lib/apt/lists/lock
-sleep 1s
-sudo apt-get update
-sleep 1s
-sudo apt-get upgrade
-sleep 3s
-# 2.安装python2,ipython2/3,pip2/3,virtualenv虚拟环境,vim git ssh mongodb数据库 mysql数据库 
-sudo apt-get -y install python ipython ipython3 python-pip python3-pip virtualenv virtualenvwrapper
-sleep 3s
-sudo apt-get -y install vim nginx curl nodejs npm git ssh
-sleep 3s
-sudo apt-get -y docker.io phantomjs redis mongodb mysql-server mysql-client libmysqlclient-dev
-sleep 3s
-sudo pip install --upgrade pip
-sudo pip3 install --upgrade pip
-source /usr/share/virtualenvwrapper/virtualenvwrapper.sh
-sleep 1s
-# 5.清理文件并重启系统生效
+# 2.清理文件并重启系统生效
 sudo apt-get update
 sudo apt-get upgrade
 rm -r master.zip ok-py-env-master oh-my-tuna.py .bash_history
